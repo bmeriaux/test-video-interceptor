c496d6e6fe7ef076f163b867637c020a44ef1384
Benoit Meriaux <benoit.meriaux@outlook.com>
2020-06-11 20:01:11 +0200

    handle absolute url in manifest for step 3, extract a RequestLogger class

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -3,6 +3,11 @@ import { URL } from 'url'
 import { IncomingHttpHeaders } from 'http'
 import { Util } from '@hapi/hapi'
 
+const MANIFEST = 'MANIFEST'
+const SEGMENT = 'SEGMENT'
+const SUBTITLE = 'SUBTITLE'
+const ABSOLUTE_URL_START_REGEX = new RegExp(/http(s?):\/\/[a-z0-9-.:]+/gi)
+
 export interface Interceptor {
   execute(urlPath: string, headers: Util.Dictionary<string>): Promise<Interceptor.Response>
 }
@@ -12,14 +17,36 @@ export namespace Interceptor {
     headers: IncomingHttpHeaders,
     statusCode: number
   }
+  export class RequestLogger {
+    private start: Date | undefined
+    constructor (private mediaContext:string, private url: string, private headers: Util.Dictionary<string>) {
+    }
+
+    logRequestIn () {
+      if (this.headers.range?.startsWith('bytes=0')) {
+        console.log('[TRACK SWITCH]')
+      }
+      this.start = new Date()
+      console.log(`[IN][${this.mediaContext}] ${this.url}`)
+    }
+
+    logRequestOut () {
+      const end: Date = new Date()
+      // @ts-ignore
+      console.log(`[OUT][${this.mediaContext}] ${this.url} (${end - this.start}ms)`)
+    }
+  }
   export function getMediaContext (url: string): string {
     const urlExtension = url.substring(url.lastIndexOf('.'))
     switch (urlExtension) {
-      case '.m3u8': return 'MANIFEST'
-      case '.ts': return 'SEGMENT'
-      case '.vtt': return 'SUBTITLE'
+      case '.m3u8': return MANIFEST
+      case '.ts': return SEGMENT
+      case '.vtt': return SUBTITLE
       default: return ''
     }
+  }
+  export function removeAbsoluteUrlFromManifest (manifest: Buffer): Buffer {
+    return Buffer.from(manifest.toString().replace(ABSOLUTE_URL_START_REGEX, ''))
   }
   export function factory (): Interceptor {
     return {
@@ -27,13 +54,16 @@ export namespace Interceptor {
         const targetBaseUrl = process.env.TARGET_HOST || 'https://bitdash-a.akamaihd.net'
         const targetUrl = `${targetBaseUrl}${urlPath}`
         headers.host = new URL(targetBaseUrl).host
-        console.log(`[IN][${getMediaContext(targetUrl)}] ${targetUrl}`)
-        const start: Date = new Date()
+        const mediaContext = getMediaContext(targetUrl)
+        const requestLogger = new RequestLogger(mediaContext, targetUrl, headers)
+        requestLogger.logRequestIn()
         const response: GotResponse<any> = await got.get(targetUrl, { headers })
-        const end: Date = new Date()
-        // @ts-ignore
-        console.log(`[OUT][${getMediaContext(targetUrl)}] ${targetUrl} (${end - start}ms)`)
-        return { body: response.rawBody, headers: response.headers, statusCode: response.statusCode }
+        requestLogger.logRequestOut()
+        let responseBody = response.rawBody
+        if (mediaContext === MANIFEST) {
+          responseBody = removeAbsoluteUrlFromManifest(response.rawBody)
+        }
+        return { body: responseBody, headers: response.headers, statusCode: response.statusCode }
       }
     }
   }
--- test/interceptor.test.ts
+++ test/interceptor.test.ts
@@ -1,5 +1,10 @@
 import { Interceptor } from '../src/interceptor'
+import { readFileSync } from 'fs'
+import Path from 'path'
+import { SinonFakeTimers, SinonStub } from 'sinon'
 import nock = require('nock')
+import sinon = require('sinon')
+import RequestLogger = Interceptor.RequestLogger
 
 const chai = require('chai')
 chai.use(require('sinon-chai'))
@@ -10,17 +15,98 @@ describe('Interceptor', () => {
   beforeEach(() => {
     interceptor = Interceptor.factory()
   })
+  describe('RequestLogger', () => {
+    let consoleLogStub: SinonStub
+    let requestLogger: Interceptor.RequestLogger
+    const url = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
+    beforeEach(() => {
+      consoleLogStub = sinon.stub(console, 'log')
+    })
+    afterEach(() => {
+      consoleLogStub.restore()
+    })
+    context('header range start with bytes=0', () => {
+      beforeEach(() => {
+        requestLogger = new RequestLogger('MANIFEST', url, { range: 'bytes=0' })
+      })
+      context('logRequestIn', () => {
+        beforeEach(() => {
+          requestLogger.logRequestIn()
+        })
+        it('call console.log with TRACK SWITCH', () => {
+          expect(consoleLogStub).to.have.been.calledWith('[TRACK SWITCH]')
+        })
+        it('call console.log with expected String for IN', () => {
+          expect(consoleLogStub).to.have.been.calledWith('[IN][MANIFEST] https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')
+        })
+      })
+    })
+    context('header range does not start with bytes=0', () => {
+      beforeEach(() => {
+        requestLogger = new RequestLogger('MANIFEST', url, {})
+      })
+      context('logRequestIn', () => {
+        beforeEach(() => {
+          requestLogger.logRequestIn()
+        })
+        it('call console.log with expected String for IN', () => {
+          expect(consoleLogStub)
+            .to.have.been.calledWith('[IN][MANIFEST] https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')
+        })
+      })
+      context('logRequestOut', () => {
+        let clock: SinonFakeTimers
+        beforeEach(() => {
+          const now = new Date()
+          clock = sinon.useFakeTimers(now)
+          requestLogger.logRequestIn()
+          clock.tick(2000)
+          requestLogger.logRequestOut()
+        })
+        afterEach(() => clock.restore)
+        it('call console.log with expected String for OUT with request duration', () => {
+          expect(consoleLogStub)
+            .to.have.been.calledWith('[OUT][MANIFEST] https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8 (2000ms)')
+        })
+      })
+    })
+  })
+  describe('removeAbsoluteUrlFromManifest', () => {
+    let manifest: Buffer
+    beforeEach(() => {
+      const originalManifest: Buffer = readFileSync(Path.resolve(__dirname, 'chunklist_b1128000.m3u8'))
+      manifest = Interceptor.removeAbsoluteUrlFromManifest(originalManifest)
+    })
+    it('remove scheme and hostname of absolute url', () => {
+      expect(manifest.indexOf('https://')).to.equal(-1)
+      expect(manifest.indexOf('http://')).to.equal(-1)
+      expect(manifest.indexOf('wowza-test.streamroot.io')).to.equal(-1)
+    })
+  })
   describe('execute', () => {
     const reqHeaders = { host: 'localhost:8080', 'cache-control': 'no-cache', 'user-agent': 'VLC/3.0.8 LibVLC/3.0.8' }
     const responseHeaders = { 'content-type': 'application/octet-stream', 'cache-control': 'no-cache', 'accept-ranges': 'bytes' }
     const urlPath = '/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
     let actual: Interceptor.Response
-
+    let logRequestInStub: SinonStub
+    let logRequestOutStub: SinonStub
     beforeEach(async () => {
       nock('https://bitdash-a.akamaihd.net')
         .get(urlPath)
         .reply(200, 'response', responseHeaders)
+      logRequestInStub = sinon.stub(RequestLogger.prototype, 'logRequestIn')
+      logRequestOutStub = sinon.stub(RequestLogger.prototype, 'logRequestOut')
       actual = await interceptor.execute(urlPath, reqHeaders)
+    })
+    afterEach(() => {
+      logRequestInStub.restore()
+      logRequestOutStub.restore()
+    })
+    it('log request In', () => {
+      expect(logRequestInStub).to.have.been.called
+    })
+    it('log request Out', () => {
+      expect(logRequestOutStub).to.have.been.called
     })
     it('returns a response', () => {
       const expectedReponse = { body: Buffer.from('response'), headers: responseHeaders, statusCode: 200 }
